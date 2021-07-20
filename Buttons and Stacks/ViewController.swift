38e52169826beaf62c36b38e6d48e4d85af133bc
Steven Lindauer <lindaues@bellsouth.net>
2021-07-20 11:07:49 -0400

    "finished" project...buttons lead to label color changes and the clear button works

--- Buttons and Stacks/ViewController.swift
+++ Buttons and Stacks/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    var colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .systemIndigo, .purple]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func colorButton(_ sender: UIButton) {
+        if sender.currentTitle == "Clear" {
+            colorLabel.text = ""
+            colorLabel.backgroundColor = UIColor.clear
+        } else {
+            colorLabel.text = sender.currentTitle
+            colorLabel.backgroundColor = colors[sender.tag]
+        }
+        
+    }
+    
 }
 
